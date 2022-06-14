4f8c1f18f19189117485c1cb9834f02c99da4e63
Jones-Mutua <jonhmutua27@gmail.com>
2022-06-14 10:45:19 +0300

    Created contact page and added contact forms

--- mjengo/src/pages/Contact/Contact.tsx
+++ mjengo/src/pages/Contact/Contact.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { NavBar } from '../../components/NavBar'
+import { IndexPage } from './Index'
 
 export const Contact = () => {
   return (
       <>
    <NavBar />
    <h1>Contact Us</h1>
+   <IndexPage />
    </>
   )
 }
